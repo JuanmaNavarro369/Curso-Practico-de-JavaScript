d6a2e88b755f3560a9b45dbac616158f54334a37
Juan Manuel Navarro <juanma_2_@hotmail.com>
2023-11-13 11:52:54 +0100

    Arreglos en los estilos del modo responsivo(media queries)

--- main.js
+++ main.js
@@ -158,6 +158,7 @@ productList.push({
     img: "https://images.pexels.com/photos/1983037/pexels-photo-1983037.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 });
 
+/* CREANDO EL HTML DE LA TARJETA DEL PRODUCTO */
 function renderProducts(arr){
     for (product of arr){
     const productCard = document.createElement("div");
